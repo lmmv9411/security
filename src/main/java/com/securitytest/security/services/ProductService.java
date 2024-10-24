0da4b1c727aecc5129832386655be3e6e51e668d
lmmv9411 <luismmv9411@gmail.com>
2024-10-23 20:59:15 -0500

    fix imports unnecesary

--- src/main/java/com/securitytest/security/services/ProductService.java
+++ src/main/java/com/securitytest/security/services/ProductService.java
@@ -4,13 +4,10 @@ import com.securitytest.security.models.Product;
 import com.securitytest.security.repositories.ProductRepository;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Service
 public class ProductService {
